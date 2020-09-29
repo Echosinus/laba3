d2a197e32233f94a1b925e9c7f9751f70e0be089
Echosinus <61726989+Echosinus@users.noreply.github.com>
2020-09-29 07:06:16 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,51 +1,25 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
+//очередь реализация получения/удаления элемента
+#define _CRT_SECURE_NO_WARNINGS
 #include "queue.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-struct queue; //очередь
 void main()
 {
-	char sym[N];
-	int i = 0;
-	int flag1 = 0, flag2 = 0;
-	char a;
+	int i=0;
 	struct queue* q;
 	q = malloc(sizeof(struct queue));
 	init_que(q);
 	printf("enetry element's queue\n");
-	while (i < N)
-	{
-		scanf("%c", &a);
-		if (a == ' ')
-		{
-			flag1 = 1;
-		}
-		if (a == '\n')
-		{
-			flag2 = 1;
-		}
-		if (flag1 == 0)
-		{
-			if (flag2 == 0)
-			{
-				sym[i] = a;
-				i++;
-			}
-		}
-		flag1 = 0;
-		flag2 = 0;
-	}
-	for (i = 0; i < N; i++)
-	{
-		push(q, sym[i]);
-	}
+	check(q);
 	print(q);
 	while (q->quefirst != N + 1)
 	{
-		a = pop(q);
-		printf("delet element %d\n", a);
-		print(q);
+			pop(q);
+			printf("delet element %c\n");
+			print(q);
 	}
 	system("pause");
+	free(q);
+	return 0;
 }
