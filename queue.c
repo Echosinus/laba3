d93cf1350b4b5f4640141ea69b1504bb1ea97c80
Echosinus <61726989+Echosinus@users.noreply.github.com>
2020-09-29 07:05:51 +0300

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,46 +1,76 @@
-﻿#include "queue.h"
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <math.h>
+//очередь
+#include "queue.h"
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 void init_que(struct queue* q) //инициализация очереди
 {
 	q->quefirst = 1;
 	q->quelast = 0;
-	return;
+	return 0;
 }
-void push(struct queue* q, char x) //вставка в очередь элемента
+int push(struct queue* q, char x) //вставка в очередь элемента
 {
 	if (q->quelast <= N)
 	{
 		q->quelast++;
 		q->que[q->quelast] = x;
+		return 0;
 	}
 	else
-		printf("queue is empty\n");
-	return;
+		return 1;
 }
 int is_empty(struct queue* q) //проверка очереди
 {
 	if (q->quelast < q->quefirst)
 		return 1;
-	else  return 0;
+	else  
+		return 0;
 }
-void print(struct queue* q) //вывод элементов
+int print(struct queue* q) //вывод элементов
 {
 	int h;
 	if (is_empty(q) == 1)
 	{
-		printf("queue is empty\n");
-		return;
+		return 1;
 	}
 	for (h = q->quefirst; h <= q->quelast; h++)
 		printf("%c ", q->que[h]);
-	return;
+	return 0;
 }
-int pop(struct queue* q)//удаление элементов
+int pop(struct queue* q,char x)//удаление элементов
 {
-	char x;
+	if (is_empty(q) == 1)
+	{
+		return 1;
+	}
 	x = q->que[q->quefirst];
 	q->quefirst++;
-	return x;
+	return 0;
+}
+char check(struct queue* q, char b)//проверяет на пробелы и энтеры и вводит символы в массив очереди
+{
+	int flag1 = 0, flag2 = 0, j = 0;
+	while (j < N)
+	{
+		scanf("%c", &b);
+		if (b == ' ')
+		{
+			flag1 = 1;
+		}
+		if (b == '\n')
+		{
+			flag2 = 1;
+		}
+		if (flag1 == 0)
+		{
+			if (flag2 == 0)
+			{
+				push(q,b);
+				j++;
+			}
+		}
+		flag1 = 0;
+		flag2 = 0;
+	}
+	return 0;
 }
