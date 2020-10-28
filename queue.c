e23c387f0470ee8bead483b81420d9934e5b8f2d
Echosinus <61726989+Echosinus@users.noreply.github.com>
2020-10-28 14:46:38 +0300

    Update queue.c

--- queue.c
+++ queue.c
@@ -2,75 +2,88 @@
 #include "queue.h"
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-void init_que(struct queue* q) //инициализация очереди
+
+
+//инициализация очереди
+void init_que(struct queue* q)
 {
 	q->quefirst = 1;
 	q->quelast = 0;
-	return 0;
+	return;
 }
-int push(struct queue* q, char x) //вставка в очередь элемента
+
+//вставка в очередь элемента
+int push(struct queue* q, char x,int n)
 {
-	if (q->quelast <= N)
+	if (q->quelast <= n)
 	{
 		q->quelast++;
 		q->que[q->quelast] = x;
 		return 0;
 	}
 	else
-		return 1;
+	return 1;
 }
-int is_empty(struct queue* q) //проверка очереди
+
+//проверка очереди
+int is_empty(struct queue* q)
 {
 	if (q->quelast < q->quefirst)
 		return 1;
 	else  
 		return 0;
 }
-int print(struct queue* q) //вывод элементов
+
+//вывод элементов
+int print(struct queue* q)
 {
 	int h;
 	if (is_empty(q) == 1)
-	{
 		return 1;
-	}
 	for (h = q->quefirst; h <= q->quelast; h++)
 		printf("%c ", q->que[h]);
 	return 0;
 }
-int pop(struct queue* q,char x)//удаление элементов
+
+//удаление элементов
+int pop(struct queue* q, char x)
 {
 	if (is_empty(q) == 1)
 	{
 		return 1;
 	}
+	else
+	{
 	x = q->que[q->quefirst];
 	q->quefirst++;
-	return 0;
-}
-char check(struct queue* q, char b)//проверяет на пробелы и энтеры и вводит символы в массив очереди
-{
-	int flag1 = 0, flag2 = 0, j = 0;
-	while (j < N)
-	{
-		scanf("%c", &b);
-		if (b == ' ')
-		{
-			flag1 = 1;
-		}
-		if (b == '\n')
-		{
-			flag2 = 1;
-		}
-		if (flag1 == 0)
-		{
-			if (flag2 == 0)
-			{
-				push(q,b);
-				j++;
-			}
-		}
-		flag1 = 0;
-		flag2 = 0;
 	}
 	return 0;
+}
+
+//проверяет на пробелы и энтеры и вводит символы в массив очереди
+char check(struct queue* q, char b)
+{
+	int flag = 0, j = 0, n = 1;
+	printf("enetry size queue\n");
+	if ((!scanf("%d", &n)) || n < 0)
+		return 1;
+	else
+	{
+		{
+			printf("enetry elements queue\n");
+			while (j < n)
+			{
+				scanf("%c", &b);
+				if (b == ' ' || b == '\n')
+					flag = 1;
+				if (flag == 0)
+				{
+					push(q, b, n);
+					j++;
+				}
+				flag = 0;
+			}
+			return 0;
+		}
+	}
 }
